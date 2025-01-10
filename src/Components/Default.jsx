610dceb5efc8e37fdff63620e0279bc3c6072578
Xavi-010 <josejavierchq@gmail.com>
2025-01-09 21:49:18 -0400

    labo 6

--- src/Components/Default.jsx
+++ src/Components/Default.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg?url'; 
 
+
 const Default = () =>{
     const [count, setCount] = useState(0)
 
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -9,16 +9,24 @@ import reactLogo from '../assets/react.svg';
 import viteLogo from '../assets/vite.svg'; 
 import githubLogo from '../assets/github.svg';
 
+import { useSelector, useDispatch } from 'react-redux'
+
 const Home = ()=> {
+  const count = useSelector((state) => state.counter.value)
 
   return (
     <div>
       {/* <Logo/>
       <Github/> */}
+      <div>
+        <span>Count Value: {count}</span>
+      </div>
 
       <OpenLink redirectURL="https://vite.dev" logoImage={viteLogo} altValue="Vite"/>
       <OpenLink redirectURL="https://react.dev" logoImage={reactLogo} altValue="React"/>
       <OpenLink redirectURL="https://github.com/Xavi-010/DiplomadoReactJS" logoImage={githubLogo} altValue="GitHub"/>
+
+
       <Body/>
       <Footer/>
     </div>
