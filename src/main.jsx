0db721f8410e8fee866299835bd80d226418a6ea
Xavi-010 <josejavierchq@gmail.com>
2025-01-16 20:35:21 -0400

    GitHub

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { BrowserRouter } from "react-router";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/DiplomadoReactJS/">
         <App />
       </BrowserRouter>
     </Provider>
